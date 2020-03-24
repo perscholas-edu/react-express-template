423e634fd319f3aecd3c732c006112cf6b2294bc
git-leon <xleonhunter@gmail.com>
2020-03-24 18:26:20 -0400

    issue resolved; forgot to import `Component` from React

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './customers.css';
+
 
 class Customers extends Component {
     render() {
@@ -10,5 +11,5 @@ class Customers extends Component {
         );
     }
 }
-    
+
 export default Customers;
