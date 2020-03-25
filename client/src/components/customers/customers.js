c06ee1bfe1a2dbb0764a3572d3735166994f5c26
git-leon <xleonhunter@gmail.com>
2020-03-25 11:09:03 -0400

    updated package.json

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import './customers.css';
 
-
 class Customers extends Component {
+    constructor() {
+        super();
+        this.state = {
+            customers:[]
+        }
+    }
+
+    componentDidMount() { // runs automatically when component is mounted
+        fetch('/api/customers') // fetches customers from express server and sets `state`-value to customers that were fetched
+            .then(res => res.json())
+            .then(customers => this.setState({customers}, (customers) => console.log("Customers fetched...", customers)))
+    }
+
     render() {
         return (
             <div>
-                <h2>Customers</h2>
+                Customers   
             </div>
         );
     }
